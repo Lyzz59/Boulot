72486a0895c7cb4c7426556a6702e08d6b492c42
Amelie <amelie_du89@hotmail.fr>
2022-06-21 17:01:46 +0200

    first commit

---
+++ loopback/src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './ping.controller';
+export * from './user.controller';
+export * from './role.controller';
+export * from './time-tracking.controller';
+export * from './typology.controller';
+export * from './project.controller';
